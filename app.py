7b7cf9a467a702f691ce3cb347502f923c546ee1
Ching Han Chen <ching.chen@ibm.com>
2025-06-24 18:46:38 +0200

    chore: Update port cfg
    Signed-off-by: Ching Han Chen <ching.chen@ibm.com>

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from statin_logic import get_statin_recommendation # Import the refactored logic
 
@@ -50,4 +51,4 @@ def evaluate():
 if __name__ == '__main__':
     # For development, run with debug=True.
     # In production, use a production-ready WSGI server like Gunicorn.
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
